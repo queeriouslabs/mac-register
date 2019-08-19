5c78584942a18e4f1d7add1a4475b41419b33057
Elias Lawson-Fox <me@eliaslfox.com>
2019-08-18 18:06:22 -0700

    save to people.txt file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
+const shellscape = require("shell-escape");
+
 const ip6addr = require("ip6addr");
 const { exec } = require("child_process");
+
 
 const app = express();
 app.use(morgan("dev"));
@@ -14,23 +17,33 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-        const { msg } = req.body;
-        const { remoteAddress: ip } = req.connection;
-        const addr = ip6addr.parse(ip);
-        const ipv4 = addr.toString({ format: "v4" });
+    const { msg, name } = req.body;
+    const { remoteAddress: ip } = req.connection;
+    const addr = ip6addr.parse(ip);
+    const ipv4 = addr.toString({ format: "v4" });
 
-        exec(`nmap -sn ${ipv4} | grep "MAC Address" | sed "s/MAC Address\: //"`, (err, stdout_, stderr) => {
+    exec(`nmap -sn ${ipv4} | grep "MAC Address" | sed "s/MAC Address\: //"`, (err, stdout_, stderr) => {
         if (err != null) {
-                        console.error(err);
-                        return res.status(500).send("Something went wrong.");
-                }
+            console.error(err);
+            return res.status(500).send("Something went wrong.");
+        }
 
-                const out = stdout_.toString();
-                console.log(ipv4, out);
-                const [ mac ] = out.split(" ");
+        const out = stdout_.toString();
+        console.log(ipv4, out);
+        const [ mac ] = out.split(" ");
 
-                res.send(`Registered message ${msg} for mac address ${mac}`);
+        const obj = { name, msg, mac };
+        const str = shellscape([JSON.stringify(obj)]);
+
+        exec(`echo ${str} >> people.txt`, (err, stdout_, stderr) => {
+            if (err != null) {
+                console.log(err);
+                return res.status(500).send("Something went wrong.");
+            }
+
+            res.send(`Registered message ${msg} for mac address ${mac}`);
         });
+    });
 });
 
 console.log("Server starting");
